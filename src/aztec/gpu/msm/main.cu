132201cdb3eaf2f9f00ee410e602ceda24e55ad7
Talderei <tad222@lehigh.edu>
2023-09-06 07:41:27 +0000

    preliminary work on integrating Icicle library

--- src/aztec/gpu/msm/main.cu
+++ src/aztec/gpu/msm/main.cu
@@ -7,6 +7,11 @@
 #include <polynomials/polynomial_arithmetic.hpp>
 #include <iostream>
 #include <fstream>
+#include "msm.h"
+#include "ntt.h"
+#include "projective.h"
+#include "ve_mod_mult.h"
+#include <dlfcn.h>
 
 using namespace std;
 using namespace pippenger_common;
@@ -14,35 +19,49 @@ using namespace waffle;
 using namespace barretenberg;
 
 int main(int, char**) {
-    // Initialize dynamic 'msm_t' object 
-    msm_t<point_t, scalar_t> *msm = new msm_t<point_t, scalar_t>();
     
-    // Construct elliptic curve points from SRS
-    auto reference_string = std::make_shared<waffle::FileReferenceString>(NUM_POINTS, "../srs_db/ignition");
-    g1::affine_element* points = reference_string->get_monomials();
+    void* lib_handle = dlopen("/home/ubuntu/Cuda-Barretenberg/src/aztec/gpu", RTLD_LAZY);
 
-    // Construct random scalars 
-    std::vector<fr> scalars;
-    scalars.reserve(NUM_POINTS);
-    for (size_t i = 0; i < NUM_POINTS; ++i) {
-        scalars.emplace_back(fr::random_element());
-    }
+    BN254_projective_t* (*myfunc_ptr)() = (BN254_projective_t * (*)())dlsym(lib_handle, "projective_zero_bn254");
+
+    BN254_projective_t result = (*myfunc_ptr)();
+
+    cout << "result is: " << result[0] << endl;
+
+    exit(0);
+    
+    // Initialize dynamic 'msm_t' object 
+    // msm_t<point_t, scalar_t> *msm = new msm_t<point_t, scalar_t>();
+    
+    // // Construct elliptic curve points from SRS
+    // auto reference_string = std::make_shared<waffle::FileReferenceString>(NUM_POINTS, "../srs_db/ignition");
+    // g1::affine_element* points = reference_string->get_monomials();
+
+    // // Construct random scalars 
+    // std::vector<fr> scalars;
+    // scalars.reserve(NUM_POINTS);
+    // for (size_t i = 0; i < NUM_POINTS; ++i) {
+    //     scalars.emplace_back(fr::random_element());
+    // }
+
+    // g1::affine_element* icicle_result;
+    // msm_cuda_bn254(icicle_result, points, &scalars[0], scalars.size(), 0);
 
     // Number of streams
-    int num_streams = 1;
+    // int num_streams = 1;
 
-    // Initialize dynamic pippenger 'context' object
-    Context<point_t, scalar_t> *context = msm->pippenger_initialize(points, &scalars[0], num_streams, NUM_POINTS);
+    // // Initialize dynamic pippenger 'context' object
+    // Context<point_t, scalar_t> *context = msm->pippenger_initialize(points, &scalars[0], num_streams, NUM_POINTS);
 
-    // Execute "Double-And-Add" reference kernel
-    g1_gpu::element *result_1 = msm->msm_double_and_add(context, NUM_POINTS, points, &scalars[0]);
+    // // Execute "Double-And-Add" reference kernel
+    // g1_gpu::element *result_1 = msm->msm_double_and_add(context, NUM_POINTS, points, &scalars[0]);
 
-    // Execute "Pippenger's Bucket Method" kernel
-    g1_gpu::element **result_2 = msm->msm_bucket_method(context, points, &scalars[0], num_streams);
+    // // Execute "Pippenger's Bucket Method" kernel
+    // g1_gpu::element **result_2 = msm->msm_bucket_method(context, points, &scalars[0], num_streams);
 
-    // Print results 
-    context->pipp.print_result(result_1, result_2);
+    // // Print results 
+    // context->pipp.print_result(result_1, result_2);
 
-    // Verify the final results are equal
-    context->pipp.verify_result(result_1, result_2);
+    // // Verify the final results are equal
+    // context->pipp.verify_result(result_1, result_2);
 }
--- src/aztec/gpu/plonk/queue_wrapper.cu
+++ src/aztec/gpu/plonk/queue_wrapper.cu
@@ -19,23 +19,23 @@ void QueueWrapper::process_queue() {
                     auto reference_string = std::make_shared<waffle::FileReferenceString>(NUM_POINTS, "../srs_db");
                     g1::affine_element* pointss = reference_string->get_monomials();
 
-                    cout << "points is: " << pointss[0].x.data[0] << endl;
-                    cout << "points is: " << pointss[0].x.data[1] << endl;
-                    cout << "points is: " << pointss[0].x.data[2] << endl;
-                    cout << "points is: " << pointss[0].x.data[3] << endl;
-                    cout << "points is: " << pointss[0].y.data[0] << endl;
-                    cout << "points is: " << pointss[0].y.data[1] << endl;
-                    cout << "points is: " << pointss[0].y.data[2] << endl;
-                    cout << "points is: " << pointss[0].y.data[3] << endl;
+                    // cout << "points is: " << pointss[0].x.data[0] << endl;
+                    // cout << "points is: " << pointss[0].x.data[1] << endl;
+                    // cout << "points is: " << pointss[0].x.data[2] << endl;
+                    // cout << "points is: " << pointss[0].x.data[3] << endl;
+                    // cout << "points is: " << pointss[0].y.data[0] << endl;
+                    // cout << "points is: " << pointss[0].y.data[1] << endl;
+                    // cout << "points is: " << pointss[0].y.data[2] << endl;
+                    // cout << "points is: " << pointss[0].y.data[3] << endl;
 
-                    cout << "points is: " << pointss[1].x.data[0] << endl;
-                    cout << "points is: " << pointss[1].x.data[1] << endl;
-                    cout << "points is: " << pointss[1].x.data[2] << endl;
-                    cout << "points is: " << pointss[1].x.data[3] << endl;
-                    cout << "points is: " << pointss[1].y.data[0] << endl;
-                    cout << "points is: " << pointss[1].y.data[1] << endl;
-                    cout << "points is: " << pointss[1].y.data[2] << endl;
-                    cout << "points is: " << pointss[1].y.data[3] << endl;
+                    // cout << "points is: " << pointss[1].x.data[0] << endl;
+                    // cout << "points is: " << pointss[1].x.data[1] << endl;
+                    // cout << "points is: " << pointss[1].x.data[2] << endl;
+                    // cout << "points is: " << pointss[1].x.data[3] << endl;
+                    // cout << "points is: " << pointss[1].y.data[0] << endl;
+                    // cout << "points is: " << pointss[1].y.data[1] << endl;
+                    // cout << "points is: " << pointss[1].y.data[2] << endl;
+                    // cout << "points is: " << pointss[1].y.data[3] << endl;
 
                     // Naive implementation for quick testing
                     // msm_t<g1_gpu::affine_element, scalar_t, point_t> *msm = new msm_t<g1_gpu::affine_element, fr_gpu, point_t>();
@@ -45,7 +45,7 @@ void QueueWrapper::process_queue() {
                     // Context<bucket_t, point_t, scalar_t, affine_t> *context = msm->pippenger_initialize(points);
 
                     // g1_gpu::element *final_result_2 = msm->msm_bucket_method(context, item.mul_scalars, key->reference_string->get_monomials(), 1 << 10);
-                    exit(0);
+                    // exit(0);
 
 
                     auto runtime_state = barretenberg::scalar_multiplication::pippenger_runtime_state(key->small_domain.size + 1);
